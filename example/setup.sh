9102cefd6a0de1c186599650628817d50998f683
Nicholas Ngai <nicholas.ngai@berkeley.edu>
2023-03-04 22:53:18 -0800

    Modify example to use gradescope-harden

--- example/setup.sh
+++ example/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+. /autograder/source/gradescope-harden/source/setup.sh
+
 apt-get install -y python3 python3-pip python3-dev
 
 pip3 install -r /autograder/source/requirements.txt
